e19f69b62c829a6890c632e820bbec0130a4dc8b
Greg Bowser <topnotcher@gmail.com>
2019-10-05 13:41:24 -0400

    [find] Add pack ID to display

--- app/src/main/java/io/bowsers/packlogger/FindPack.kt
+++ app/src/main/java/io/bowsers/packlogger/FindPack.kt
@@ -4,7 +4,6 @@ package io.bowsers.packlogger
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -135,6 +134,7 @@ class FindPack : Fragment() {
             }
 
             (activity!!.findViewById(R.id.displayed_pack_name) as TextView).text = pack.name
+            (activity!!.findViewById(R.id.displayed_pack_id) as TextView).text = pack.id.toString()
         }
 
 
