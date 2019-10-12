94c65daeaecc534c814b2720123efc29a850b548
Greg Bowser <topnotcher@gmail.com>
2019-10-12 01:22:35 -0400

    [packlist] Avoid loading pack list multiple times.
    The autocompleters needed to wait for this data in a separate thread, so
    they were just synchronously loading their own copies. Instead allow the
    load to be initiated from any thread and add a condition variable for
    the autocompleters to wait for the data to load.

--- app/src/main/java/io/bowsers/packlogger/PackList.kt
+++ app/src/main/java/io/bowsers/packlogger/PackList.kt
@@ -2,8 +2,11 @@ package io.bowsers.packlogger
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class PackList(private val loader: SheetsCollectionLoader) : ViewModel() {
     class Factory(private val loader: SheetsCollectionLoader) : ViewModelProvider.Factory {
@@ -16,11 +19,16 @@ class PackList(private val loader: SheetsCollectionLoader) : ViewModel() {
 
     private val packs: MutableLiveData<List<PackData>> by lazy {
         MutableLiveData<List<PackData>>().also {
-            table.select().let {
-                it.setResultCallback(this::postValue)
-            }.executeInBackground()
+            loadPacksOnce()
         }
     }
+
+    private var packList: List<PackData>? = null
+
+    private var loadStarted = false
+    private val lock = ReentrantLock()
+    private val condition = lock.newCondition()
+    private var loaded = false
 
     private val table by lazy {
         loader.table<PackData>("all-packs!A2:B").apply {
@@ -33,15 +41,53 @@ class PackList(private val loader: SheetsCollectionLoader) : ViewModel() {
         }
     }
 
+    private fun shouldLoad() : Boolean {
+        var load = false
+        synchronized(loadStarted) {
+            if (!loadStarted) {
+                loadStarted = true
+                load = true
+            }
+        }
+        return load
+    }
+
+    private fun loadPacksOnce() {
+        if (shouldLoad()) {
+            doLoad()
+        }
+    }
+
+    private fun doLoad() {
+        table.select()
+            .setResultCallback(this::postValue)
+            .executeInBackground()
+    }
+
     fun getPacks(): LiveData<List<PackData>> {
         return packs
     }
 
     fun getDataSynchronous(): List<PackData> {
-        return table.select().execute()
+        loadPacksOnce()
+
+        lock.withLock {
+            if (!loaded) condition.await()
+        }
+
+        return packList!!
     }
 
     private fun postValue(value: List<PackData>) {
         packs.postValue(value)
+
+        // postValue posts to the GUI thread. We can't guarantee packs.value is set when any waiters of
+        // condition wake up, so set the data in a separate property as well.
+        packList = value
+
+        lock.withLock {
+            loaded = true
+            condition.signalAll()
+        }
     }
 }
